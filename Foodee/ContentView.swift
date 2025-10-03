13d56f13c8b08c15f57f4e4bf91b6c67ac009faa
chengjun wang <ll_wcj@163.com>
2025-10-03 09:37:58 +0200

    Initial Commit

---
+++ Foodee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Foodee
+//
+//  Created by 黄硕 on 03.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Foodee/FoodeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodeeApp.swift
+//  Foodee
+//
+//  Created by 黄硕 on 03.10.25.
+//
+
+import SwiftUI
+
+@main
+struct FoodeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
